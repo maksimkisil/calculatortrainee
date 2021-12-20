1c8241001362d0766e51ae496219f44ea2cc26bf
maksimkisil <maksimkisil164@gmail.com>
2021-12-21 01:09:14 +0300

    Updates

---
+++ precache-manifest.f882c9e435b3df1f93e8887d5cc8590d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88ed42356f05cb72bdcd",
+    "url": "/calculatortrainee/static/css/main.1b7f4c0b.chunk.css"
+  },
+  {
+    "revision": "88ed42356f05cb72bdcd",
+    "url": "/calculatortrainee/static/js/main.85eaf331.chunk.js"
+  },
+  {
+    "revision": "7100189c45d17ab4a986",
+    "url": "/calculatortrainee/static/js/runtime~main.affbf371.js"
+  },
+  {
+    "revision": "166b378090def73254dd",
+    "url": "/calculatortrainee/static/js/2.f3afe8eb.chunk.js"
+  },
+  {
+    "revision": "1edb5c3c562faeff7c8eecb8a5b121ab",
+    "url": "/calculatortrainee/index.html"
+  }
+];
